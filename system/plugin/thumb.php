bbd1a59170ea28c3c6398bead88e5f92212f6d89
artygrand <mail@artygrand.ru>
2013-07-10 19:22:54 +0700

    Add crop function by default

--- system/plugin/thumb.php
+++ system/plugin/thumb.php
@@ -2,12 +2,15 @@
 class SimpleImage{
 	public $image;
 	public $image_type;
+	public $width;
+	public $height;
  
-	public function load($filename) {
+	public function load($filename){
 		if (!is_file($filename)){
 			header("HTTP/1.0 404 Not Found");
 			die;
 		}
+		
 		$image_info = getimagesize($filename);
 		$this->image_type = $image_info[2];
 		if($this->image_type == IMAGETYPE_JPEG ){
@@ -17,7 +20,11 @@ class SimpleImage{
 		} elseif( $this->image_type == IMAGETYPE_PNG ){
 			$this->image = imagecreatefrompng($filename);
 		}
+		
+		$this->width = imagesx($this->image);
+		$this->height = imagesy($this->image);
 	}
+
 	public function save($filename, $image_type=IMAGETYPE_JPEG, $compression=75, $permissions=0777) {
 		if( $image_type == IMAGETYPE_JPEG ) {
 			imagejpeg($this->image,$filename,$compression);
@@ -30,34 +37,37 @@ class SimpleImage{
 			chmod($filename,$permissions);
 		}
 	}
-	public function getWidth() {
-		return imagesx($this->image);
-	}
-	public function getHeight() {
-		return imagesy($this->image);
-	}
-	public function needResize($width,$height){
-		if($width / $this->getWidth() < $height / $this->getHeight()){
-			$this->resizeToWidth($width);
+
+	public function resize($thumb_width, $thumb_height){
+		if($thumb_width / $this->width < $thumb_height / $this->height){
+			$new_width = $thumb_width;
+			$new_height = $this->height * ($thumb_width / $this->width);
 		} else {
-			$this->resizeToHeight($height);
+			$new_width = $this->width * ($thumb_height / $this->height);
+			$new_height = $thumb_height;
 		}
-	}
-	public function resizeToHeight($height) {
-		$ratio = $height / $this->getHeight();
-		$width = $this->getWidth() * $ratio;
-		$this->resize($width,$height);
-	}
-	public function resizeToWidth($width) {
-		$ratio = $width / $this->getWidth();
-		$height = $this->getheight() * $ratio;
-		$this->resize($width,$height);
-	}
-	public function resize($width,$height) {
-		$new_image = imagecreatetruecolor($width, $height);
-		imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+		$new_image = imagecreatetruecolor($new_width, $new_height);
+		imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $new_width, $new_height, $this->width, $this->height);
 		$this->image = $new_image;
 	}
+
+	public function crop($thumb_width, $thumb_height){
+		if ($this->width / $this->height >= $thumb_width / $thumb_height){
+			$new_width = $this->width / ($this->height / $thumb_height);
+			$new_height = $thumb_height;
+		} else {
+			$new_width = $thumb_width;
+			$new_height = $this->height / ($this->width / $thumb_width);
+		}
+		$new_image = imagecreatetruecolor($thumb_width, $thumb_height);
+		imagecopyresampled($new_image, $this->image, 0 - ($new_width - $thumb_width) / 2, 0 - ($new_height - $thumb_height) / 2, 0, 0, $new_width, $new_height, $this->width, $this->height);
+		$this->image = $new_image;
+	}
+
+	public function doit($what, $width, $height){
+		$this -> $what($width, $height);
+	}
+
 	public function destroy() {
 		imagedestroy($this->image);
 	}
@@ -69,7 +79,7 @@ if (isset($_GET['src']) and $_GET['src']){
 	if (!is_file($cachedImg)){
 		$image = new SimpleImage();	
 		$image->load($_GET['src']);
-		$image->needResize((int)$_GET['width'], (int)$_GET['height']);
+		$image->doit('crop', (int)$_GET['width'], (int)$_GET['height']);
 		$image->save($cachedImg);
 		$image->destroy();
 	}
