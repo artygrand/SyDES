d627207ffe27163897bb4e2e3e0c8679f442c527
artygrand <mail@artygrand.ru>
2013-09-12 14:12:06 +0700

    add disallow rule to robots.txt for yandex
    yandex sux

--- system/plugin/robots.php
+++ system/plugin/robots.php
@@ -3,9 +3,12 @@ $pos = stripos($_SERVER["HTTP_HOST"], 'www.');
 $host = $pos !== false ? substr('$_SERVER["HTTP_HOST"]', 4) : $_SERVER["HTTP_HOST"];
 $robots = 'User-agent: *
 Disallow: /?*
+Disallow: /index.php?*
 Sitemap: http://' . $host . '/sitemap.xml
 
 User-agent: Yandex
+Disallow: /?*
+Disallow: /index.php?*
 Host: ' . $host;
 header("Content-Type: text/plain");
 die ($robots);
