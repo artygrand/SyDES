1b579f55f32ddbd965b17ebc4558322b5b62f37e
artygrand <mail@artygrand.ru>
2013-09-23 16:55:52 +0700

    fix bug

--- system/plugin/robots.php
+++ system/plugin/robots.php
@@ -1,6 +1,6 @@
 <?php
 $pos = stripos($_SERVER["HTTP_HOST"], 'www.');
-$host = $pos !== false ? substr('$_SERVER["HTTP_HOST"]', 4) : $_SERVER["HTTP_HOST"];
+$host = $pos !== false ? substr($_SERVER["HTTP_HOST"], 4) : $_SERVER["HTTP_HOST"];
 $robots = 'User-agent: *
 Disallow: /?*
 Disallow: /index.php?*
