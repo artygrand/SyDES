11bacb944d0e34c9b088a5f9323d043fd67f3b27
Arthur Grand <artygrand@gmail.com>
2015-05-05 22:14:27 +0600

    probably not

--- system/widget/lastpages.php
+++ system/widget/lastpages.php
@@ -20,7 +20,7 @@ class LastpagesWidget extends Widget{
 			);
 		}
 		$this->title = t('widget_lastpages');
-		$this->content = H::table($rows, false, 'class="table lastpages"');
+		$this->content = H::table($rows, array(), 'class="table lastpages"');
 		$this->style = ".lastpages td:nth-child(1){max-width:200px;white-space:nowrap;text-overflow:ellipsis;overflow:hidden;}.lastpages td:nth-child(2){width:100px;}.lastpages td:nth-child(3){text-align:right;width:80px;}";
 		$this->wrap_body = false;
 		return $this->render();
