8d3170b5da26c4b4bd9d61bca195199c14413a4f
Arthur Grand <artygrand@gmail.com>
2015-05-24 21:21:00 +0600

    Fixed bug import tool
    line endings and quotes escaping

--- system/module/tools/import.php
+++ system/module/tools/import.php
@@ -108,7 +108,7 @@ class ImportController extends Controller{
 		$this->response->addHeader("Content-Disposition: attachment;filename=syd_{$t}_" . date("Ymd") . ".{$_POST['encoding']}.csv");
 		$t = '';
 		foreach ($titles as $row){
-			$t .= '"' . implode('";"', str_replace('"', '""', $row)) . '"' . PHP_EOL;
+			$t .= '"' . implode('";"', str_replace(array('&quot;', "\r\n"), array('""', "\n"), $row)) . '"' . "\r\n";
 		}
 		if ($_POST['encoding'] == 'cp1251'){
 			$t = iconv('utf-8', 'cp1251//TRANSLIT', $t);
@@ -139,7 +139,7 @@ class ImportController extends Controller{
 		}
 		set_time_limit(0);
 
-		$arr = explode(PHP_EOL, $csv);
+		$arr = explode("\r\n", $csv);
 		$rows = array();
 		foreach ($arr as $r){
 			$rows[] = str_getcsv($r, ';', '"');
