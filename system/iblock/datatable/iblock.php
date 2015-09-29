25cc24f9997dac0f4563e8e6c5000f210324aa11
Arthur Grand <artygrand@gmail.com>
2015-09-29 18:50:54 +0600

    Added filter to iblock dataTable

--- system/iblock/datatable/iblock.php
+++ system/iblock/datatable/iblock.php
@@ -3,6 +3,7 @@
 $defaults = array(
 	'limit' => 20,
 	'order' => 'id DESC',
+	'filter' => '',
 	'show_pagination' => 1,
 );
 $args = array_merge($defaults, $args);
@@ -11,7 +12,16 @@ if (!isset($args['show'])){
 	return;
 }
 
-$stmt = $this->db->query("SELECT count(*) FROM {$args['show']} WHERE status = 1");
+$filter = array(
+	'status = 1'
+);
+if ($args['filter']){
+	$part = explode('=', $args['filter']);
+	$filter[] = "{$part[0]} = '{$part[1]}'";
+}
+$filter = implode(' AND ', $filter);
+
+$stmt = $this->db->query("SELECT count(*) FROM {$args['show']} WHERE {$filter}");
 $count = $stmt->fetchColumn();
 if (!$count){
 	return;
@@ -19,7 +29,7 @@ if (!$count){
 
 $skip = (isset($_GET['skip']) && (int)$_GET['skip'] > 0 && $args['show_pagination']) ? (int)$_GET['skip'] : 0;
 
-$stmt = $this->db->query("SELECT * FROM {$args['show']} WHERE status = 1 ORDER BY {$args['order']} LIMIT {$skip}, {$args['limit']}");
+$stmt = $this->db->query("SELECT * FROM {$args['show']} WHERE {$filter} ORDER BY {$args['order']} LIMIT {$skip}, {$args['limit']}");
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 array_walk_recursive($result, function(&$value){
