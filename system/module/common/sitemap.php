567e9bf403407156b442ac978b9d2e332af53fd8
Arthur Grand <artygrand@gmail.com>
2015-10-03 21:48:36 +0600

    Improved sitemap in multilingual sites
    All pages now shown instead of only current locale

--- system/module/common/sitemap.php
+++ system/module/common/sitemap.php
@@ -11,23 +11,27 @@ class SitemapController extends Controller{
 	public static $front = array('index');
 	
 	public function index(){
-		$sitemap = $this->cache->get($this->site . '.' . $this->locale . '.' . 'sitemap');
+		$sitemap = $this->cache->get($this->site . '.sitemap');
 		if (!$sitemap){
-			$stmt = $this->db->query("SELECT p.path, p.cdate FROM pages p, pages_content pc
-			WHERE p.status > 0 AND pc.page_id = p.id AND pc.locale = '{$this->locale}' AND pc.title != '' ORDER BY p.path");
-			$pages = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			$pages = array();
 
-			if (!$pages){
-				return;
+			foreach ($this->config_site['locales'] as $locale){
+				$stmt = $this->db->query("SELECT p.path, p.cdate FROM pages p, pages_content pc
+				WHERE p.status > 0 AND pc.page_id = p.id AND pc.locale = '{$locale}' AND pc.title != '' ORDER BY p.path");
+				$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+				$in_url = count($this->config_site['locales']) > 1 ? $locale : '';
+				foreach ($data as $item){
+					$item['path'] = $item['path'] == '/' ? $in_url : ($in_url ? $in_url . $item['path'] : substr($item['path'], 1));
+					$pages[] = $item;
+				}
 			}
 
-			$in_url = count($this->config_site['locales']) > 1 ? $this->locale : '';
 			$sitemap = '<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" href="/system/module/common/assets/xml-sitemap.xsl"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
 			foreach ($pages as $page){
-				$page['path'] = $page['path'] == '/' ? $in_url : ($in_url ? $in_url . $page['path'] : substr($page['path'], 1));
 				$sitemap .= '
 	<url>
 		<loc>http://' . $this->base . '/' . $page['path'] . '</loc>
@@ -38,7 +42,7 @@ class SitemapController extends Controller{
 			}
 
 			$sitemap .= PHP_EOL . '</urlset>';
-			$this->cache->set($this->site . '.' . $this->locale . '.' . 'sitemap', $sitemap, 2592000);
+			$this->cache->set($this->site . '.sitemap', $sitemap, 2592000);
 		}
 
 		$this->response->mime = 'xml';
